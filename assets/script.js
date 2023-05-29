19ec6ce75c6b4e2cf87d5fc7eb98bff901e3ad65
Avery Myers <averyjmyers@hotmail.com>
2023-05-29 10:36:19 -0700

    html buttons and quiz questions

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var quizQuestions = [
+    {
+      question: "What is the result of 2 + 2?",
+      options: ["3", "4", "5", "6"],
+      answer: "4"
+    },
+    {
+      question: "Which operator is used for string concatenation?",
+      options: ["+", "*", "-", "/"],
+      answer: "+"
+    },
+    {
+      question: "What is the output of the following code?\n\nconsole.log(3 > 2);",
+      options: ["true", "false"],
+      answer: "true"
+    },
+    {
+      question: "Which keyword is used to declare a function in JavaScript?",
+      options: ["function", "var", "let", "const"],
+      answer: "function"
+    },
+    {
+      question: "What is the correct way to write a comment in JavaScript?",
+      options: ["<!-- This is a comment -->", "// This is a comment", "/* This is a comment */", "<-- This is a comment -->"],
+      answer: "// This is a comment"
+    }
+  ];
